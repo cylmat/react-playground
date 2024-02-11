d6c9d1981b2e8daedfde64f4e646346238674d21
CyrilMatte <2111517-CyrilMatte@users.noreply.replit.com>
2024-02-11 19:55:19 +0000

    config

--- src/page/App.tsx
+++ src/page/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import CssSample from "../components/CssSample";
+import CssSample from "../components/Css/CssSample";
 import Logo from "../components/Logo";
-import Tabs from "../components/Tabs";
+import Tabs from "../components/Tab/Tabs";
 import reactLogo from "../assets/react.svg";
 import viteLogo from "/favicon.svg";
 import "./App.css";
